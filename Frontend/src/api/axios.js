6d46b8eaf2dffb4ba3363cc9c6c66d70da512af4
nodecraftsolutions-lang <nodecraftsolutions@gmail.com>
2025-09-21 17:28:53 +0530

    Update axios.js

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: "https://mocktest-bckx.onrender.com/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
